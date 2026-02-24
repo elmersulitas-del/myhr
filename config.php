64998a787769b8334ae95be6e60d4d6844af4116
xjm20 <pesalbonjames98@gmail.com>
2026-02-24 15:52:25 +0800

    udpdate login

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ session_start();
 
 define('GOOGLE_CLIENT_ID', '114629538600-4enr9va17bt69r21nqp7594vlmou60aa.apps.googleusercontent.com');
 
-//define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_CLIENT_SECRET', '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');
 
 // IMPORTANT: Must exactly match your "Authorized redirect URI"
 define('GOOGLE_REDIRECT_URI', 'http://localhost/myhr/google_callback.php');
